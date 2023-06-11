49df95ad228374acd50c09e15e0096e7f4ff2f30
Andreas Rumpf <rumpf_a@web.de>
2023-06-11 07:55:58 +0200

    better semver test; WIP (#16)
    * WIP
    
    * added missing files
    
    * test the locally built atlas.exe
    
    * progress
    
    * fixes atlas path
    
    * remove exporting path
    
    ---------
    
    Co-authored-by: ringabout <43030857+ringabout@users.noreply.github.com>

--- tests/tester.nim
+++ tests/tester.nim
@@ -1,6 +1,6 @@
 # Small program that runs the test cases
 
-import std / [strutils, os, sequtils]
+import std / [strutils, os, sequtils, strformat]
 from std/private/gitutils import diffFiles
 
 if execShellCmd("nim c -r src/versions.nim") != 0:
@@ -8,12 +8,9 @@ if execShellCmd("nim c -r src/versions.nim") != 0:
 
 var failures = 0
 
-when defined(develop):
-  const atlasExe = "bin" / "atlas".addFileExt(ExeExt)
-  if execShellCmd("nim c -o:$# src/atlas.nim" % [atlasExe]) != 0:
-    quit("FAILURE: compilation of atlas failed")
-else:
-  const atlasExe = "atlas".addFileExt(ExeExt)
+let atlasExe = absolutePath("bin" / "atlas".addFileExt(ExeExt))
+if execShellCmd("nim c -o:$# src/atlas.nim" % [atlasExe]) != 0:
+  quit("FAILURE: compilation of atlas failed")
 
 proc exec(cmd: string) =
   if execShellCmd(cmd) != 0:
@@ -41,6 +38,61 @@ proc testWsConflict() =
   removeDir("tests/ws_conflict/cpkg")
   removeDir("tests/ws_conflict/dpkg")
   removeDir(myproject)
+
+type
+  Node = object
+    name: string
+    versions: seq[string]
+    deps: seq[string]
+
+proc createNode(name: string, versions: seq[string], deps: seq[string]): Node =
+  result = Node(name: name, versions: versions, deps: deps)
+
+template withDir(dir: string; body: untyped) =
+  let old = getCurrentDir()
+  try:
+    setCurrentDir(dir)
+    body
+  finally:
+    setCurrentDir(old)
+
+proc createNimblePackage(node: Node) =
+  let packagePath = node.name
+  createDir(packagePath)
+  withDir packagePath:
+    var nimbleContent = &"""
+# Nimble package file for {node.name}
+author = "Your Name"
+license = "MIT"
+
+# Dependencies
+    """
+    for d in node.deps:
+      nimbleContent.add &"requires \"{d}\""
+    writeFile(node.name & ".nimble", nimbleContent)
+
+proc testSemVer() =
+  # Example graph data
+  var graph: seq[Node] = @[
+    createNode("A", @["1.0.0", "1.1.0", "2.0.0"], @[]),
+    createNode("B", @["2.1.0", "3.0.0", "3.1.0"], @["A >= 1.0.0"]),
+    createNode("C", @["1.2.0", "1.2.1"], @["B >= 2.0.0"]),
+    createNode("D", @["1.0.0", "1.1.0", "1.1.1"], @[]),
+    createNode("E", @["2.0.0", "2.0.1", "2.1.0"], @["D >= 1.0.0"]),
+    createNode("F", @["1.0.0", "1.0.1", "1.1.0"], @["E >= 2.0.0"]),
+  ]
+
+  createDir "source"
+  withDir "source":
+    for i in 0..<graph.len:
+      createNimblePackage graph[i]
+
+  createDir "myproject"
+  withDir "myproject":
+    exec atlasExe & " --showGraph use F"
+
+withDir "tests/ws_semver":
+  testSemVer()
 
 testWsConflict()
 if failures > 0: quit($failures & " failures occurred.")
