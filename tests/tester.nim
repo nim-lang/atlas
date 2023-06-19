a21a63f1dc5b150f75adf15264d6e8e271a05ec7
araq <rumpf_a@web.de>
2023-06-19 06:58:11 +0200

    progress

--- tests/tester.nim
+++ tests/tester.nim
@@ -127,11 +127,11 @@ proc testSemVer2() =
       exec "git init"
       writeFile "proj_b.nimble", "requires \"proj_c >= 1.0.0\"\n"
       exec "git add proj_b.nimble"
-      exec "git commit -m 'Initial commit for project B'"
+      exec "git commit -m " & quoteShell("Initial commit for project B")
 
       writeFile "proj_b.nimble", "requires \"proj_c >= 1.1.0\"\n"
       exec "git add proj_b.nimble"
-      exec "git commit -m 'Update proj_b.nimble for project B'"
+      exec "git commit -m " & quoteShell("Update proj_b.nimble for project B")
       exec "git tag v1.1.0"
 
     createDir "proj_c"
@@ -139,7 +139,7 @@ proc testSemVer2() =
       exec "git init"
       writeFile "proj_c.nimble", "requires \"proj_d >= 1.2.0\"\n"
       exec "git add proj_c.nimble"
-      exec "git commit -m 'Initial commit for project C'"
+      exec "git commit -m " & quoteShell("Initial commit for project C")
       writeFile "proj_c.nimble", "requires \"proj_d >= 1.2.0\"\n"
       exec "git tag v1.2.0"
 
@@ -148,11 +148,11 @@ proc testSemVer2() =
       exec "git init"
       writeFile "proj_d.nimble", "\n"
       exec "git add proj_d.nimble"
-      exec "git commit -m 'Initial commit for project D'"
+      exec "git commit -m " & quoteShell("Initial commit for project D")
       exec "git tag v1.0.0"
       writeFile "proj_d.nimble", "requires \"does_not_exist >= 1.2.0\"\n"
       exec "git add proj_d.nimble"
-      exec "git commit -m 'broken version of package D'"
+      exec "git commit -m " & quoteShell("broken version of package D")
 
       exec "git tag v2.0.0"
 
@@ -164,7 +164,7 @@ proc testSemVer2() =
         discard "fine"
       else:
         echo "expected ", SemVer2ExpectedResult, " but got ", outp
-        raise newException(AssertionError, "Test failed!")
+        raise newException(AssertionDefect, "Test failed!")
     else:
       assert false, outp
 
