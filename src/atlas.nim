7b780811a168f3f32bff4822369dda46a7f87f9a
Andreas Rumpf <rumpf_a@web.de>
2023-06-27 12:36:05 +0200

    version update (#46)

--- src/atlas.nim
+++ src/atlas.nim
@@ -17,7 +17,7 @@ import context, runners, osutils, packagesjson, sat, gitops, nimenv, lockfiles,
 export osutils, context
 
 const
-  AtlasVersion = "0.6.0"
+  AtlasVersion = "0.6.2"
   LockFileName = "atlas.lock"
   Usage = "atlas - Nim Package Cloner Version " & AtlasVersion & """
 
