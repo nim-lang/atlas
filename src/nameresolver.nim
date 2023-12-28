5f2011d5641404e3647844e33bb2d0ab9ff197c4
Araq <rumpf_a@web.de>
2023-12-28 10:17:29 +0100

    bugfix

--- src/nameresolver.nim
+++ src/nameresolver.nim
@@ -125,7 +125,7 @@ proc dependencyDir*(c: var AtlasContext; pkg: Package): PackageDir =
     return PackageDir pkg.path.string.absolutePath
   if c.workspace.lastPathComponent == pkg.repo.string:
     debug c, pkg, "dependencyDir: workspace: " & c.workspace
-    return PackageDir getCurrentDir()
+    return PackageDir c.workspace
 
   if pkg.path.string.len() > 0:
     checkDir pkg.path.string
