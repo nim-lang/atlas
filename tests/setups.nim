c711d8ec92f65f08f1c53a2ab0aa48d4e61bead6
Andreas Rumpf <rumpf_a@web.de>
2024-01-22 16:19:56 +0100

    added test for 'detect versions from Nimble file' feature (#106)

--- tests/setups.nim
+++ tests/setups.nim
@@ -65,5 +65,49 @@ proc buildGraph* =
 
       exec "git tag v2.0.0"
 
+proc buildGraphNoGitTags* =
+  createDir "source"
+  withDir "source":
+
+    createDir "proj_a"
+    withDir "proj_a":
+      exec "git init"
+      writeFile "proj_a.nimble", "version = \"1.0.0\"\n\nrequires \"proj_b >= 1.0.0\"\n"
+      exec "git add proj_a.nimble"
+      exec "git commit -m 'update'"
+      writeFile "proj_a.nimble", "version = \"1.1.0\"\n\nrequires \"proj_b >= 1.1.0\"\n"
+      exec "git add proj_a.nimble"
+      exec "git commit -m 'update'"
+
+    createDir "proj_b"
+    withDir "proj_b":
+      exec "git init"
+      writeFile "proj_b.nimble", "version = \"1.0.0\"\n\nrequires \"proj_c >= 1.0.0\"\n"
+      exec "git add proj_b.nimble"
+      exec "git commit -m " & quoteShell("Initial commit for project B")
+
+      writeFile "proj_b.nimble", "version = \"1.1.0\"\n\nrequires \"proj_c >= 1.1.0\"\n"
+      exec "git add proj_b.nimble"
+      exec "git commit -m " & quoteShell("Update proj_b.nimble for project B")
+
+    createDir "proj_c"
+    withDir "proj_c":
+      exec "git init"
+      writeFile "proj_c.nimble", "version = \"1.0.0\"\n\nrequires \"proj_d >= 1.2.0\"\n"
+      exec "git add proj_c.nimble"
+      exec "git commit -m " & quoteShell("Initial commit for project C")
+      writeFile "proj_c.nimble", "version = \"1.2.0\"\n\nrequires \"proj_d >= 1.0.0\"\n"
+      exec "git commit -am " & quoteShell("Update proj_c.nimble for project C")
+
+    createDir "proj_d"
+    withDir "proj_d":
+      exec "git init"
+      writeFile "proj_d.nimble", "version = \"1.0.0\"\n\n"
+      exec "git add proj_d.nimble"
+      exec "git commit -m " & quoteShell("Initial commit for project D")
+      writeFile "proj_d.nimble", "version = \"2.0.0\"\n\nrequires \"does_not_exist >= 1.2.0\"\n"
+      exec "git add proj_d.nimble"
+      exec "git commit -m " & quoteShell("broken version of package D")
+
 when isMainModule:
   buildGraph()
