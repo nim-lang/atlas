2d1589b68306489a4fa9910a553678a93134149e
Jaremy Creechley <creechley@gmail.com>
2025-02-08 07:05:42 -0700

    reorganize a bit

--- src/lockfiles.nim
+++ src/lockfiles.nim
@@ -15,52 +15,6 @@ import context, gitops, nimblechecksums, compilerversions,
 const
   NimbleLockFileName* = "nimble.lock"
 
-type
-  LockFileEntry* = object
-    dir*: string
-    url*: string
-    commit*: string
-    version*: string
-
-  LockedNimbleFile* = object
-    filename*: string
-    content*: seq[string]
-
-  LockFile* = object # serialized as JSON
-    items*: OrderedTable[string, LockFileEntry]
-    nimcfg*: seq[string]
-    nimbleFile*: LockedNimbleFile
-    hostOS*, hostCPU*: string
-    nimVersion*, gccVersion*, clangVersion*: string
-
-proc convertKeyToArray(jsonTree: var JsonNode, path: varargs[string]) =
-  var parent: JsonNode
-  var content: JsonNode = jsonTree
-  for key in path:
-    if content.hasKey(key):
-      parent = content
-      content = parent[key]
-    else:
-      return
-
-  if content.kind == JString:
-    var contents = newJArray()
-    for line in content.getStr.split("\n"):
-      contents.add(% line)
-    parent[path[^1]] = contents
-
-proc readLockFile(filename: string): LockFile =
-  let jsonAsStr = readFile(filename)
-  var jsonTree = parseJson(jsonAsStr)
-
-  # convert older non-array file contents to JArray
-  jsonTree.convertKeyToArray("nimcfg")
-  jsonTree.convertKeyToArray("nimbleFile", "content")
-  result = jsonTo(jsonTree, LockFile,
-    Joptions(allowExtraKeys: true, allowMissingKeys: true))
-
-proc write(lock: LockFile; lockFilePath: string) =
-  writeFile lockFilePath, toJson(lock).pretty
 
 proc prefixedPath*(c: var AtlasContext, path: string): string =
   let parts = path.splitPath
@@ -245,7 +199,6 @@ proc convertNimbleLock*(c: var AtlasContext; nimblePath: string): LockFile =
         url: pkgurl,
         commit: info["vcsRevision"].getStr
       )
-
 
 proc convertAndSaveNimbleLock*(c: var AtlasContext; nimblePath, lockFilePath: string) =
   ## convert and save a nimble.lock into an Atlast lockfile
