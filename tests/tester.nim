f1b5f08240a20e043101d96b5644b3fdf1be0787
Jaremy Creechley <creechley@gmail.com>
2025-02-09 08:44:10 -0700

    adding proxy

--- tests/tester.nim
+++ tests/tester.nim
@@ -189,7 +189,9 @@ if failures > 0: quit($failures & " failures occurred.")
 # Normal: create or remotely cloning repos
 # nim c -r   1.80s user 0.71s system 60% cpu 4.178 total
 # shims/nim c -r   32.00s user 25.11s system 41% cpu 2:18.60 total
+# nim c -r   30.83s user 24.67s system 40% cpu 2:17.17 total
 
 # Local repos:
 # nim c -r   1.59s user 0.60s system 88% cpu 2.472 total
 # w/integration: nim c -r   23.86s user 18.01s system 71% cpu 58.225 total
+# w/integration: nim c -r   32.00s user 25.11s system 41% cpu 1:22.80 total
