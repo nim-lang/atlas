4c26259400675f6e1cb0e0e0b6d2f2abbb9a1bb7
Jaremy Creechley <creechley@gmail.com>
2025-02-09 03:34:42 -0700

    rework setup a bit

--- tests/githttp.nim
+++ tests/githttp.nim
@@ -7,9 +7,24 @@ type
     path: string
     name: string
 
+var searchDirs: seq[string]
+
+proc findDir(org, repo, files: string): string =
+  {.cast(gcsafe).}:
+    for folder in searchDirs:
+      result = folder / org / repo / files
+      if dirExists(folder / org / repo):
+        break
+
 proc handleRequest(req: Request) {.async.} =
-  let path = getCurrentDir() / req.url.path.strip(chars={'/'})
-  echo "http request: ", req.reqMethod, " path: ", path
+  let arg = req.url.path.strip(chars={'/'})
+  let dirs = arg.split('/')
+  let org = dirs[0]
+  let repo = dirs[1]
+  let files = dirs[2..^1].join($DirSep)
+  let path = findDir(org, repo, files)
+
+  echo "http request: ", req.reqMethod, " repo: ", repo, " path: ", path
 
   # Serve static files if not a git request
   if fileExists(path):
@@ -26,8 +41,13 @@ proc handleRequest(req: Request) {.async.} =
     await req.respond(Http404, "File not found")
 
 when isMainModule:
+
   let server = newAsyncHttpServer()
   let port = 8080
   
+  for arg in commandLineParams():
+    if dirExists(arg):
+      searchDirs.add(arg)
+
   echo "Starting server on port ", port
   waitFor server.serve(Port(port), handleRequest)
