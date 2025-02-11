00f6fedc4a7812b27def5ef4dc356c632520c516
Jaremy Creechley <creechley@gmail.com>
2025-02-11 03:52:56 -0700

    reorg tests a bit

--- test-repos/createTestReposZip.nim
+++ test-repos/createTestReposZip.nim
@@ -1,4 +1,4 @@
-import std/os
+import std/[os, strutils]
 
 import wsGenerated
 import wsIntegration
@@ -10,4 +10,21 @@ proc exec(cmd: string) =
   if execShellCmd(cmd) != 0:
     quit "FAILURE: " & cmd
 
-exec "ls -lh"
+template withDir(dir: string; body: untyped) =
+  let old = getCurrentDir()
+  try:
+    setCurrentDir(dir)
+    # echo "WITHDIR: ", dir, " at: ", getCurrentDir()
+    body
+  finally:
+    setCurrentDir(old)
+
+let nimble = readFile("atlas.nimble")
+var ver = ""
+for line in nimble.split("\n"):
+  if line.startsWith("version ="):
+    ver = line.replace("\"", "").split("=")[1]
+
+withDir "test-repos":
+  doAssert ver != "" and "." in ver, "need to provide atlas version"
+  exec "zip -r test-repos.zip ws_generated/ ws_integrated/"
