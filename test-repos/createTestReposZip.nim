26b6d888a5e8c318e8d3e2faf0dcc33fdccf6a2b
Jaremy Creechley <creechley@gmail.com>
2025-02-11 03:54:16 -0700

    reorg tests a bit

--- test-repos/createTestReposZip.nim
+++ test-repos/createTestReposZip.nim
@@ -24,7 +24,8 @@ var ver = ""
 for line in nimble.split("\n"):
   if line.startsWith("version ="):
     ver = line.replace("\"", "").split("=")[1]
+doAssert ver != "" and "." in ver and ver.len() < 10, "need to provide atlas version"
 
 withDir "test-repos":
-  doAssert ver != "" and "." in ver, "need to provide atlas version"
+  exec "rm test-repos.zip"
   exec "zip -r test-repos.zip ws_generated/ ws_integrated/"
