2986b374342abc1c84290b71e0226b6d17d8d2a3
Jaremy Creechley <creechley@gmail.com>
2025-03-07 00:57:00 -0700

    rebase

--- src/nimenv.nim
+++ src/nimenv.nim
@@ -31,74 +31,75 @@ proc infoAboutActivation(nimDest: Path, nimVersion: string) =
     info nimDest, "RUN\nsource nim-" & nimVersion & "/activate.sh"
 
 proc setupNimEnv*(workspace: Path, nimVersion: string; keepCsources: bool) =
-  template isDevel(nimVersion: string): bool = nimVersion == "devel"
+  when false:
+    template isDevel(nimVersion: string): bool = nimVersion == "devel"
 
-  template exec(command: string) =
-    let cmd = command # eval once
-    if os.execShellCmd(cmd) != 0:
-      error ("nim-" & nimVersion), "failed: " & cmd
-      return
-
-  let nimDest = Path("nim-" & nimVersion)
-  if dirExists($(workspace / nimDest)):
-    if not fileExists($(workspace / nimDest / ActivationFile)):
-      info nimDest, "already exists; remove or rename and try again"
-    else:
-      infoAboutActivation nimDest, nimVersion
-    return
-
-  var major, minor, patch: int
-  if nimVersion != "devel":
-    if not scanf(nimVersion, "$i.$i.$i", major, minor, patch):
-      error "nim", "cannot parse version requirement"
-      return
-  let csourcesVersion =
-    if nimVersion.isDevel or (major == 1 and minor >= 9) or major >= 2:
-      # already uses csources_v2
-      "csources_v2"
-    elif major == 0:
-      "csources" # has some chance of working
-    else:
-      "csources_v1"
-  withDir $workspace:
-    # if not dirExists(csourcesVersion):
-    #   exec "git clone https://github.com/nim-lang/" & csourcesVersion
-    # exec "git clone https://github.com/nim-lang/nim " & $nimDest
-  withDir $workspace / csourcesVersion:
-    when defined(windows):
-      exec "build.bat"
-    else:
-      let makeExe = findExe("make")
-      if makeExe.len == 0:
-        exec "sh build.sh"
-      else:
-        exec "make"
-  let nimExe0 = ".." / csourcesVersion / "bin" / "nim".addFileExt(ExeExt)
-  let dir = Path(workspace / nimDest)
-  withDir $(workspace / nimDest):
-    let nimExe = "bin" / "nim".addFileExt(ExeExt)
-    copyFileWithPermissions nimExe0, nimExe
-    let query = createQueryEq(if nimVersion.isDevel: Version"#head" else: Version(nimVersion))
-    if not nimVersion.isDevel:
-      let commit = versionToCommit(dir, SemVer, query)
-      if commit.len == 0:
-        error nimDest, "cannot resolve version to a commit"
+    template exec(command: string) =
+      let cmd = command # eval once
+      if os.execShellCmd(cmd) != 0:
+        error ("nim-" & nimVersion), "failed: " & cmd
         return
-      discard checkoutGitCommit(dir, commit)
-    exec nimExe & " c --noNimblePath --skipUserCfg --skipParentCfg --hints:off koch"
-    let kochExe = when defined(windows): "koch.exe" else: "./koch"
-    exec kochExe & " boot -d:release --skipUserCfg --skipParentCfg --hints:off"
-    exec kochExe & " tools --skipUserCfg --skipParentCfg --hints:off"
-    # remove any old atlas binary that we now would end up using:
-    if cmpPaths(getAppDir(), $(workspace / nimDest / "bin".Path)) != 0:
-      removeFile "bin" / "atlas".addFileExt(ExeExt)
-    # unless --keep is used delete the csources because it takes up about 2GB and
-    # is not necessary afterwards:
-    if not keepCsources:
-      removeDir $workspace / csourcesVersion / "c_code"
-    let pathEntry = workspace / nimDest / "bin".Path
-    when defined(windows):
-      writeFile "activate.bat", BatchFile % $pathEntry.replace('/', '\\')
-    else:
-      writeFile "activate.sh", ShellFile % $pathEntry
-    infoAboutActivation nimDest, nimVersion
+
+    let nimDest = Path("nim-" & nimVersion)
+    if dirExists($(workspace / nimDest)):
+      if not fileExists($(workspace / nimDest / ActivationFile)):
+        info nimDest, "already exists; remove or rename and try again"
+      else:
+        infoAboutActivation nimDest, nimVersion
+      return
+
+    var major, minor, patch: int
+    if nimVersion != "devel":
+      if not scanf(nimVersion, "$i.$i.$i", major, minor, patch):
+        error "nim", "cannot parse version requirement"
+        return
+    let csourcesVersion =
+      if nimVersion.isDevel or (major == 1 and minor >= 9) or major >= 2:
+        # already uses csources_v2
+        "csources_v2"
+      elif major == 0:
+        "csources" # has some chance of working
+      else:
+        "csources_v1"
+    withDir $workspace:
+      if not dirExists(csourcesVersion):
+        exec "git clone https://github.com/nim-lang/" & csourcesVersion
+      exec "git clone https://github.com/nim-lang/nim " & $nimDest
+    withDir $workspace / csourcesVersion:
+      when defined(windows):
+        exec "build.bat"
+      else:
+        let makeExe = findExe("make")
+        if makeExe.len == 0:
+          exec "sh build.sh"
+        else:
+          exec "make"
+    let nimExe0 = ".." / csourcesVersion / "bin" / "nim".addFileExt(ExeExt)
+    let dir = Path(workspace / nimDest)
+    withDir $(workspace / nimDest):
+      let nimExe = "bin" / "nim".addFileExt(ExeExt)
+      copyFileWithPermissions nimExe0, nimExe
+      let query = createQueryEq(if nimVersion.isDevel: Version"#head" else: Version(nimVersion))
+      if not nimVersion.isDevel:
+        let commit = versionToCommit(dir, SemVer, query)
+        if commit.len == 0:
+          error nimDest, "cannot resolve version to a commit"
+          return
+        discard checkoutGitCommit(dir, commit)
+      exec nimExe & " c --noNimblePath --skipUserCfg --skipParentCfg --hints:off koch"
+      let kochExe = when defined(windows): "koch.exe" else: "./koch"
+      exec kochExe & " boot -d:release --skipUserCfg --skipParentCfg --hints:off"
+      exec kochExe & " tools --skipUserCfg --skipParentCfg --hints:off"
+      # remove any old atlas binary that we now would end up using:
+      if cmpPaths(getAppDir(), $(workspace / nimDest / "bin".Path)) != 0:
+        removeFile "bin" / "atlas".addFileExt(ExeExt)
+      # unless --keep is used delete the csources because it takes up about 2GB and
+      # is not necessary afterwards:
+      if not keepCsources:
+        removeDir $workspace / csourcesVersion / "c_code"
+      let pathEntry = workspace / nimDest / "bin".Path
+      when defined(windows):
+        writeFile "activate.bat", BatchFile % $pathEntry.replace('/', '\\')
+      else:
+        writeFile "activate.sh", ShellFile % $pathEntry
+      infoAboutActivation nimDest, nimVersion
