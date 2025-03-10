01186dd559599aea6f2b14d14563299154c6f097
Jaremy Creechley <creechley@gmail.com>
2025-03-10 03:42:33 -0600

    cleanup handling of file - fix pkgUrl

--- tests/testsemverUnit.nim
+++ tests/testsemverUnit.nim
@@ -95,7 +95,7 @@ suite "graph solve":
     """.parseTaggedVersions(false)
     let projDtags = projDnimbles.filterIt(it.v.string != "")
 
-  test "ws_semver_unit traverseDependency":
+  test "expand using http urls":
       # setAtlasVerbosity(Info)
       withDir "tests/ws_semver_unit":
         removeDir("deps")
@@ -108,6 +108,92 @@ suite "graph solve":
         nc.put("proj_b", parseUri "https://example.com/buildGraph/proj_b")
         nc.put("proj_c", parseUri "https://example.com/buildGraph/proj_c")
         nc.put("proj_d", parseUri "https://example.com/buildGraph/proj_d")
+
+        let dir = paths.getCurrentDir().absolutePath
+
+        var graph = dir.expand(nc, AllReleases, onClone=DoClone)
+
+        let sp = graph.pkgs.values().toSeq()
+        let sp0: Package = sp[0] # proj ws_testtraversal
+        let vt = toVersionTag
+        testRequirements(sp0, @[vt"#head@-"], [
+          ("https://example.com/buildGraph/proj_a", "*"),
+        ])
+
+        let sp1: Package = sp[1] # proj A
+        testRequirements(sp1, projAtags, [
+          ("https://example.com/buildGraph/proj_b", ">= 1.1.0"),
+          ("https://example.com/buildGraph/proj_b", ">= 1.0.0"),
+        ])
+        let sp2 = sp[2] # proj B
+        testRequirements(sp2, projBtags, [
+          ("https://example.com/buildGraph/proj_c", ">= 1.1.0"),
+          ("https://example.com/buildGraph/proj_c", ">= 1.0.0"),
+        ])
+        let sp3 = sp[3] # proj C
+        testRequirements(sp3, projCtags, [
+          ("https://example.com/buildGraph/proj_d", ">= 1.0.0"),
+        ])
+        let sp4 = sp[4] # proj C
+        testRequirements(sp4, projDtags, [
+          ("https://example.com/buildGraph/does_not_exist", ">= 1.2.0"),
+          ("", ""),
+        ], true)
+
+        echo "\tgraph:\n", graph.toJson(ToJsonOptions(enumMode: joptEnumString))
+
+        let form = graph.toFormular(SemVer)
+        context().dumpGraphs = true
+        var sol: Solution
+        solve(graph, form)
+
+        check graph.root.active
+        check graph.pkgs[nc.createUrl("proj_a")].active
+        check graph.pkgs[nc.createUrl("proj_b")].active
+        check graph.pkgs[nc.createUrl("proj_c")].active
+        check graph.pkgs[nc.createUrl("proj_d")].active
+
+        check $graph.root.activeVersion == "#head@-"
+        check $graph.pkgs[nc.createUrl("proj_a")].activeVersion == "1.1.0@fb3804df"
+        check $graph.pkgs[nc.createUrl("proj_b")].activeVersion == "1.1.0@ee875bae"
+        check $graph.pkgs[nc.createUrl("proj_c")].activeVersion == "1.2.0@9331e14f"
+        check $graph.pkgs[nc.createUrl("proj_d")].activeVersion == "1.0.0@0dec9c97"
+
+        let formMinVer = graph.toFormular(MinVer)
+        context().dumpGraphs = true
+        var solMinVer: Solution
+        solve(graph, formMinVer)
+
+        check $graph.root.activeVersion == "#head@-"
+        check $graph.pkgs[nc.createUrl("proj_a")].activeVersion == "1.0.0@e479b438"
+        check $graph.pkgs[nc.createUrl("proj_b")].activeVersion == "1.0.0@af427510"
+        check $graph.pkgs[nc.createUrl("proj_c")].activeVersion == "1.2.0@9331e14f"
+        check $graph.pkgs[nc.createUrl("proj_d")].activeVersion == "1.0.0@0dec9c97"
+
+        check graph.validateDependencyGraph()
+        let topo = graph.toposorted()
+
+        check topo[0].url.projectName == "proj_d"
+        check topo[1].url.projectName == "proj_c"
+        check topo[2].url.projectName == "proj_b"
+        check topo[3].url.projectName == "proj_a"
+
+        for pkg in topo:
+          echo "PKG: ", pkg.url.projectName
+
+  test "expand using buildGraphNoGitTags":
+      # setAtlasVerbosity(Info)
+      withDir "tests/ws_semver_unit":
+        removeDir("deps")
+        workspace() = paths.getCurrentDir()
+        context().flags = {UsesOverrides, KeepWorkspace, ListVersions, FullClones}
+        context().defaultAlgo = SemVer
+
+        var nc = createNimbleContext()
+        nc.put("proj_a", parseUri "https://example.com/buildGraphNoGitTags/proj_a")
+        nc.put("proj_b", parseUri "https://example.com/buildGraphNoGitTags/proj_b")
+        nc.put("proj_c", parseUri "https://example.com/buildGraphNoGitTags/proj_c")
+        nc.put("proj_d", parseUri "https://example.com/buildGraphNoGitTags/proj_d")
 
         let dir = paths.getCurrentDir().absolutePath
 
