9526a7e2eb4ae0c362aefb4fe571559fd490e9d6
Jaremy Creechley <creechley@gmail.com>
2025-03-13 17:50:30 +0200

    cleanup handling of file - add unit tests

--- tests/tnimbleparser.nim
+++ tests/tnimbleparser.nim
@@ -6,8 +6,13 @@ import basic/parse_requires
 
 import testerutils
 
+proc doesContain(res: NimbleFileInfo, name: string): bool =
+  for req in res.requires:
+    if req.contains(name):
+      result = true
+
 suite "nimbleparser":
-  test "parse nimble file":
+  test "parse nimble file when not defined(windows)":
     let nimbleFile = Path("tests" / "test_data" / "jester.nimble")
     var res = extractRequiresInfo(nimbleFile)
     echo "Nimble release: ", repr res
@@ -15,8 +20,14 @@ suite "nimbleparser":
       for req in res.requires:
         check not req.contains("httpbeast")
     else:
-      var hasHttpbeast = false
+      check doesContain(res, "httpbeast")
+
+  test "parse nimble file when defined(windows)":
+    let nimbleFile = Path("tests" / "test_data" / "jester_inverted.nimble")
+    var res = extractRequiresInfo(nimbleFile)
+    echo "Nimble release: ", repr res
+    when defined(windows):
       for req in res.requires:
-        if req.contains("httpbeast"):
-          hasHttpbeast = true
-      check hasHttpbeast
+        check not req.contains("httpbeast")
+    else:
+      check doesContain(res, "httpbeast")
