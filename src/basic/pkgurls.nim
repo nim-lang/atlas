ebdf1216166746bc94a5f30ebc8a47af9b8d2461
Jaremy Creechley <creechley@gmail.com>
2025-03-15 17:53:29 +0200

    windows fix change

--- src/basic/pkgurls.nim
+++ src/basic/pkgurls.nim
@@ -110,8 +110,7 @@ proc createUrlSkipPatterns*(raw: string, skipDirTest = false): PkgUrl =
           ("file://" & raw)
       when defined(windows):
         if raw.startsWith("file://"):
-          raw.replace(":\\", "/")
-          raw.replace(DirSep, AltSep)
+          raw.replace(AltSep, DirSep) # make it all windows style
       let u = parseUri(raw)
       result = PkgUrl(qualifiedName: extractProjectName(u), u: u, hasShortName: true)
     else:
