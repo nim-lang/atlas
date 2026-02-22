ff1f4289482dce94ba9f95b3b0ae16d16e21eb3d
Jaremy Creechley <creechley@gmail.com>
2026-02-22 14:39:17 -0700

    fix nimAtlasBootstrap (#190)
    * fix nimAtlasBootstrap
    
    * v0.10.1 - fix nimAtlasBootstrap

--- src/basic/sattypes.nim
+++ src/basic/sattypes.nim
@@ -1,5 +1,8 @@
 
-import sat/[sat, satvars]
+when defined(nimAtlasBootstrap):
+  import ../../dist/sat/src/sat/[sat, satvars]
+else:
+  import sat/[sat, satvars]
 
 proc `$`*(v: VarId): string =
   "v" & $v.int
